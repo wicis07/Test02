24c0a6253d3684367b72addbdc6f1045d6ab02e3
ernaldewi <erna.listianadewi@gmail.com>
2023-09-06 11:24:39 +0700

    Add my own HTML, CSS, JS and some assets

---
+++ source/script.js
@@ -0,0 +1,29 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// SET INTERVAL FOR CAROUSEL SERVIVES
+
+const carousel = document.querySelector(".carousel");
+const services = document.querySelectorAll(".service");
+const serviceCount = services.length;
+
+let currentIndex = 0;
+const intervalTime = 5000; // Change slide every 5 seconds
+
+function nextService() {
+  services[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % serviceCount;
+  services[currentIndex].classList.add("active");
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const translateXValue = -currentIndex * 100;
+  carousel.style.transform = `translateX(${translateXValue}%)`;
+}
+
+setInterval(nextService, intervalTime);
