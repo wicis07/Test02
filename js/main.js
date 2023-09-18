ac45734edb67992ac937eaf97f8a8b6998c3f026
Wisis Arif Setiawan <142713321+wicis07@users.noreply.github.com>
2023-09-18 17:21:25 +0700

    Add files via upload
    update gabungan styling (about,index,plan-your-visit).

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Navbar Menu Button
+var navbarNav = document.getElementById("dropdown"),
+  menuButton = document.getElementById("menu");
+
+menuButton.onclick = function () {
+  navbarNav.classList.toggle("open");
+};
+
+// SET INTERVAL FOR CAROUSEL SERVIVES
+
+const carousel = document.querySelector(".carousel");
+const services = document.querySelectorAll(".service");
+const serviceCount = services.length;
+
+let currentIndex = 0;
+const intervalTime = 5000; // Change slide every 5 seconds
+
+function nextService() {
+  services[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % serviceCount;
+  services[currentIndex].classList.add("active");
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const translateXValue = -currentIndex * 100;
+  carousel.style.transform = `translateX(${translateXValue}%)`;
+}
+
+setInterval(nextService, intervalTime);
+
+// Accordion
+var acc = document.getElementsByClassName("label");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "rem";
+    }
+  });
+}
